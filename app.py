2674d9d5b885927f25d62fe721e82f292cc1b376
yankesswang <43935115+yankesswang@users.noreply.github.com>
2024-04-08 23:49:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,61 +1,85 @@
+# Standard library imports
+import os
+import json
+from pathlib import Path
+from pprint import pprint
+import tempfile
+
+# Third-party library imports for environment management and web application
+from dotenv import load_dotenv
+import uvicorn
+import streamlit as st
+
+# FastAPI for web server
 from fastapi import FastAPI
-from langchain_core.prompts import ChatPromptTemplate
+from langserve import add_routes
+# Langchain imports for conversational retrieval and embeddings
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_community.llms import Ollama, openai
 from langchain_core.runnables import ConfigurableField
-from langchain_openai import ChatOpenAI
-from langserve import add_routes
-import os
-from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI, OpenAI, OpenAIEmbeddings
+from langchain.chains import ConversationalRetrievalChain
+from langchain_community.document_loaders import JSONLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 
-load_dotenv()
+# Llama Index imports for document processing and vector storage
+from llama_index.core.node_parser import SimpleNodeParser
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
 
-os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
-os.environ["LANGCHAIN_TRACING_V2"]="true"
-os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
-os.environ['HF_TOKEN'] = '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'
+# Streamlit components
+from streamlit_chat import message
 
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
+# Transformers for NLP models and tokenization
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 
-base_model_id = "arthurwangheng/Llama-2-7b-FAQ-chatbot-finetune"
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.bfloat16
-)
+# Environment variable setup
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ['HF_TOKEN'] = os.getenv('HUGGINGFACEHUB_API_TOKEN')
 
-model = AutoModelForCausalLM.from_pretrained(
-    base_model_id,
-    quantization_config=bnb_config,  
-    device_map="auto",
-    trust_remote_code=True,
-    use_auth_token=True
-)
+def load_llm():
+    base_model_id = "arthurwangheng/Llama-2-7b-FAQ-chatbot-finetune"
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16
+    )
 
-prompt = ChatPromptTemplate.from_messages([
-    ('system', 'You are a Emmorce FAQ assistant.You are going to answer the question customer asks'),
-    ('user', '{input}'),
-])
+    model = AutoModelForCausalLM.from_pretrained(
+        base_model_id,
+        quantization_config=bnb_config,  
+        device_map="auto",
+        trust_remote_code=True,
+        use_auth_token=True
+    )
+    return model
 
 app = FastAPI(
     title="LangChain Server",
     version="1.0",
-    description="A simple api server using Langchain's Runnable interfaces",
+    description="A simple api server for FAQ ChatBot",
 )
 
-chain = prompt | model
+def Runnable():
+    llm = load_llm()
+    prompt = ChatPromptTemplate.from_messages([
+        ('system', 'You are a Emmorce FAQ assistant.You are going to answer the question customer asks'),
+        ('user', '{input}'),
+    ])
+    chain = prompt | llm 
+    return chain
 
 add_routes(
     app,
-    chain,
-    path="/chatbot",
+    Runnable(),
+    path="/chatbot"
 )
-
 
 if __name__ == "__main__":
     import uvicorn
-
-    uvicorn.run(app, host="localhost", port=9000)
+    uvicorn.run(app, host="localhost", port=1900)
